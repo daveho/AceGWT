b3f25ee001b1fe4728a0edaf917b500d1cf3730e
ouq77 <louw77@gmail.com>
2012-06-06 15:37:56 +1200

    Exposed extra functionality
    setAnnotations()
    setHighlightSelectedWord()

--- AceGWT/src/edu/ycp/cs/dh/acegwt/client/ace/AceEditor.java
+++ AceGWT/src/edu/ycp/cs/dh/acegwt/client/ace/AceEditor.java
@@ -1,15 +1,15 @@
 // Copyright (c) 2011-2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,22 +21,25 @@
 package edu.ycp.cs.dh.acegwt.client.ace;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RequiresResize;
 
 /**
  * A GWT widget for the Ajax.org Code Editor (ACE).
- * 
+ *
  * @see <a href="http://ace.ajax.org/">Ajax.org Code Editor</a>
  */
 public class AceEditor extends Composite implements RequiresResize {
 	// Used to generate unique element ids for Ace widgets.
 	private static int nextId = 0;
-	
-	private String elementId;
-	
+
+	private final String elementId;
+
 	private JavaScriptObject editor;
+	
+  private JsArray<AceAnnotation> annotations = JavaScriptObject.createArray().cast();
 	
 	/**
 	 * This constructor will only work if the <code>.ace_editor</code>
@@ -49,24 +52,24 @@ public class AceEditor extends Composite implements RequiresResize {
 	public AceEditor() {
 		this(false);
 	}
-	
+
 	/**
 	 * Preferred constructor.
 	 * You should pass <code>true</code> to this constructor,
 	 * unless you did something special to redefine the <code>.ace_editor</code>
 	 * CSS class.
-	 * 
+	 *
 	 * @param positionAbsolute true if the <code>.ace_editor</code> CSS class
 	 *        is set with <code>position: absolute;</code>, which is
 	 *        the default; false if <code>.ace_editor</code> is set to
 	 *        use <code>position: relative;</code>
 	 */
-	public AceEditor(boolean positionAbsolute) {
+	public AceEditor(final boolean positionAbsolute) {
 		elementId = "_aceGWT" + nextId;
 		nextId++;
-		
+
 		HTML html;
-		
+
 		if (!positionAbsolute) {
 			// Create a single div with width/height 100% with the generated
 			// element id.  The ACE editor will replace this div.
@@ -85,28 +88,28 @@ public class AceEditor extends Composite implements RequiresResize {
 					"</div>"
 					);
 		}
-		
+
 		initWidget(html);
 	}
-	
+
 	/**
 	 * Call this method to start the editor.
 	 * Make sure that the widget has been attached to the DOM tree
 	 * before calling this method.
 	 */
 	public native void startEditor() /*-{
-		var editor =
-			$wnd.ace.edit(this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::elementId);
+		var editor = $wnd.ace.edit(this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::elementId);
+		editor.getSession().setUseWorker(false);
 		this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor = editor;
 
 		// I have been noticing sporadic failures of the editor
 		// to display properly and receive key/mouse events.
 		// Try to force the editor to resize and display itself fully.  See:
 		//    https://groups.google.com/group/ace-discuss/browse_thread/thread/237262b521dcea33
-		//editor.resize();
+		editor.resize();
 		this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::redisplay();
 	}-*/;
-	
+
 	/**
 	 * Call this to force the editor contents to be redisplayed.
 	 * There seems to be a problem when an AceEditor is embedded in a LayoutPanel:
@@ -119,22 +122,31 @@ public class AceEditor extends Composite implements RequiresResize {
 		var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
 		editor.renderer.onResize(true);
 		editor.renderer.updateFull();
+		editor.resize();
 		editor.focus();
 	}-*/;
-	
+
+	/**
+	 * Cleans up the entire editor.
+	 */
+	public native void destroy() /*-{
+	  var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
+    editor.destroy();
+	}-*/;
+
 	/**
 	 * Set the theme.
-	 * 
+	 *
 	 * @param theme the theme (one of the values in the {@link AceEditorTheme}
 	 *              enumeration)
 	 */
-	public void setTheme(AceEditorTheme theme) {
+	public void setTheme(final AceEditorTheme theme) {
 		setThemeByName(theme.getName());
 	}
-	
+
 	/**
 	 * Set the theme by name.
-	 * 
+	 *
 	 * @param themeName the theme name (e.g., "twilight")
 	 */
 	public native void setThemeByName(String themeName) /*-{
@@ -144,17 +156,17 @@ public class AceEditor extends Composite implements RequiresResize {
 
 	/**
 	 * Set the mode.
-	 * 
+	 *
 	 * @param mode the mode (one of the values in the
 	 *             {@link AceEditorMode} enumeration)
 	 */
-	public void setMode(AceEditorMode mode) {
+	public void setMode(final AceEditorMode mode) {
 		setModeByName(mode.getName());
 	}
-	
+
 	/**
 	 * Set the mode by name.
-	 * 
+	 *
 	 * @param shortModeName name of mode (e.g., "eclipse")
 	 */
 	public native void setModeByName(String shortModeName) /*-{
@@ -163,10 +175,10 @@ public class AceEditor extends Composite implements RequiresResize {
 		var TheMode = $wnd.require(modeName).Mode;
 		editor.getSession().setMode(new TheMode());
 	}-*/;
-	
+
 	/**
 	 * Register a handler for change events generated by the editor.
-	 * 
+	 *
 	 * @param callback the change event handler
 	 */
 	public native void addOnChangeHandler(AceEditorCallback callback) /*-{
@@ -175,10 +187,10 @@ public class AceEditor extends Composite implements RequiresResize {
 			callback.@edu.ycp.cs.dh.acegwt.client.ace.AceEditorCallback::invokeAceCallback(Lcom/google/gwt/core/client/JavaScriptObject;)(e);
 		});
 	}-*/;
-	
+
 	/**
 	 * Register a handler for cursor position change events generated by the editor.
-	 * 
+	 *
 	 * @param callback the cursor position change event handler
 	 */
 	public native void addOnCursorPositionChangeHandler(AceEditorCallback callback) /*-{
@@ -187,7 +199,7 @@ public class AceEditor extends Composite implements RequiresResize {
 			callback.@edu.ycp.cs.dh.acegwt.client.ace.AceEditorCallback::invokeAceCallback(Lcom/google/gwt/core/client/JavaScriptObject;)(e);
 		});
 	}-*/;
-	
+
 	/**
 	 * Set font size.
 	 */
@@ -196,40 +208,40 @@ public class AceEditor extends Composite implements RequiresResize {
 		var elt = $doc.getElementById(elementId);
 		elt.style.fontSize = fontSize;
 	}-*/;
-	
+
 	/**
 	 * Get the complete text in the editor as a String.
-	 * 
+	 *
 	 * @return the text in the editor
 	 */
 	public native String getText() /*-{
 		var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
 		return editor.getSession().getValue();
 	}-*/;
-	
+
 	/**
 	 * Set the complete text in the editor from a String.
-	 * 
+	 *
 	 * @param text the text to set in the editor
 	 */
 	public native void setText(String text) /*-{
 		var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
 		editor.getSession().setValue(text);
 	}-*/;
-	
+
 	/**
 	 * Insert given text at the cursor.
-	 * 
+	 *
 	 * @param text text to insert at the cursor
 	 */
 	public native void insertAtCursor(String text) /*-{
 		var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
 		editor.insert(text);
 	}-*/;
-	
+
 	/**
 	 * Get the current cursor position.
-	 * 
+	 *
 	 * @return the current cursor position
 	 */
 	public native AceEditorCursorPosition getCursorPosition() /*-{
@@ -237,34 +249,34 @@ public class AceEditor extends Composite implements RequiresResize {
 		var pos = editor.getCursorPosition();
 		return this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::getCursorPositionImpl(DD)(pos.row, pos.column);
 	}-*/;
-	
-	private AceEditorCursorPosition getCursorPositionImpl(double row, double column) {
+
+	private AceEditorCursorPosition getCursorPositionImpl(final double row, final double column) {
 		return new AceEditorCursorPosition((int) row, (int) column);
 	}
-	
+
 	/**
 	 * Set whether or not soft tabs should be used.
-	 * 
+	 *
 	 * @param useSoftTabs true if soft tabs should be used, false otherwise
 	 */
 	public native void setUseSoftTabs(boolean useSoftTabs) /*-{
 		var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
 		editor.getSession().setUseSoftTabs(useSoftTabs);
 	}-*/;
-	
+
 	/**
 	 * Set tab size.  (Default is 4.)
-	 * 
+	 *
 	 * @param tabSize the tab size to set
 	 */
 	public native void setTabSize(int tabSize) /*-{
 		var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
 		editor.getSession().setTabSize(tabSize);
 	}-*/;
-	
+
 	/**
 	 * Go to given line.
-	 * 
+	 *
 	 * @param line the line to go to
 	 */
 	public native void gotoLine(int line) /*-{
@@ -274,7 +286,7 @@ public class AceEditor extends Composite implements RequiresResize {
 
 	/**
 	 * Set whether or not the horizontal scrollbar is always visible.
-	 * 
+	 *
 	 * @param hScrollBarAlwaysVisible true if the horizontal scrollbar is always
 	 *                                visible, false if it is hidden when not needed
 	 */
@@ -282,20 +294,20 @@ public class AceEditor extends Composite implements RequiresResize {
 		var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
 		editor.renderer.setHScrollBarAlwaysVisible(hScrollBarAlwaysVisible);
 	}-*/;
-	
+
 	/**
-	 * Se whether or not the gutter is shown.
-	 * 
+	 * Set whether or not the gutter is shown.
+	 *
 	 * @param showGutter true if the gutter should be shown, false if it should be hidden
 	 */
 	public native void setShowGutter(boolean showGutter) /*-{
 		var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
 		editor.renderer.setShowGutter(showGutter);
 	}-*/;
-	
+
 	/**
 	 * Set or unset read-only mode.
-	 * 
+	 *
 	 * @param readOnly true if editor should be set to readonly, false if the
 	 *                 editor should be set to read-write
 	 */
@@ -303,16 +315,64 @@ public class AceEditor extends Composite implements RequiresResize {
 		var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
 		editor.setReadOnly(readOnly);
 	}-*/;
-	
+
+	/**
+	 * Set or unset highlighting of currently selected word.
+	 * 
+	 * @param highlightSelectedWord true to highlight currently selected word, false otherwise
+	 */
+	public native void setHighlightSelectedWord(boolean highlightSelectedWord) /*-{
+	  var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
+    editor.setHighlightSelectedWord(highlightSelectedWord);
+	}-*/;
+
 	/**
 	 * Set or unset the visibility of the print margin.
-	 * 
+	 *
 	 * @param showPrintMargin true if the print margin should be shown, false otherwise
 	 */
 	public native void setShowPrintMargin(boolean showPrintMargin) /*-{
 		var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
 		editor.renderer.setShowPrintMargin(showPrintMargin);
 	}-*/;
+	
+	/**
+	 * Adds an annotation to a the local <code>annotations</code> JsArray<AceAnnotation>, but does not set it on the editor
+	 * 
+	 * @param row to which the annotation should be added
+	 * @param column to which the annotation applies
+	 * @param text to display as a tooltip with the annotation
+	 * @param type one of "error", "warning" or "information" (which determines the icon shown)
+	 */
+	public void addAnnotation(int row, int column, String text, String type) {
+	  annotations.push(AceAnnotation.create(row, column, text, type));
+	}
+
+	/**
+	 * Sets any annotations which have been added via <code>addAnnotation</code> on the editor
+	 */
+	public native void setAnnotations() /*-{
+	  var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
+	  var annotations = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::annotations;
+	  editor.getSession().setAnnotations(annotations);
+	}-*/;
+	
+	
+  /**
+   * Clears any annotations from the editor and resets the local <code>annotations</code> JsArray<AceAnnotation>
+   */
+	public native void clearAnnotations() /*-{
+	  var editor = this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::editor;
+    editor.getSession().clearAnnotations();
+    this.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::resetAnnotations();
+	}-*/;
+	
+	/**
+	 * Resets any annotations in the local <code>annotations</code> JsArray<AceAnnotation>
+	 */
+	private void resetAnnotations() {
+	  annotations = JavaScriptObject.createArray().cast();
+	}
 	
 	/* (non-Javadoc)
 	 * @see com.google.gwt.user.client.ui.ResizeComposite#onResize()
