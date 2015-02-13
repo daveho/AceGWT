0d47948a7a1cc5520b7110a5b3fb9a211fb755d2
Tikhonov Dmitry <chitzkoy@gmail.com>
2015-02-13 08:34:38 +0300

    Project structure refactored. Maven support added.

---
+++ AceGWT/src/main/java/edu/ycp/cs/dh/acegwt/client/ace/AceSelectionListener.java
@@ -0,0 +1,13 @@
+package main.java.edu.ycp.cs.dh.acegwt.client.ace;
+
+/**
+ * Listener for selection change events.
+ */
+public interface AceSelectionListener {
+	/**
+	 * Emitted when the cursor selection changes.
+	 * 
+	 * @param selection the {@link AceSelection}
+	 */
+	public void onChangeSelection(AceSelection selection);
+}
